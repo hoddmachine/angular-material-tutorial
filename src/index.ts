ae6f8cad17af7961fe4a700f249ca92269401e51
Hordur Ragnarsson <hragnarsson@fc-md.umd.edu>
2016-06-17 13:25:34 -0400

    Added base content to tutorial

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@
 
 import * as angular from 'angular';
 import 'angular-ui-router';
+import 'angular-aria';
+import 'angular-animate';
+import 'angular-material';
+import 'angular-material-icons';
+
+import 'angular-material/angular-material.css';
 
 import { routing } from './index.config';
 
@@ -9,6 +15,10 @@ import { homeModule } from './Home';
 
 const requires: string[] = [
     'ui.router',
+    'ngAnimate',
+    'ngAria',
+    'ngMaterial',
+    'ngMdIcons',
     homeModule
 ];
 
