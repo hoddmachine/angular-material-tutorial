e5fabf526c44b949a1a0e63f416682bd55ad1a0b
hoddmachine <hordur.ragnarsson@gmail.com>
2016-06-17 13:52:24 -0400

    Fixed errors in home controller

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -44,23 +44,23 @@ export class HomeCtrl {
         ];
         this.cards = [
             {
-                img: 'https://scontent.xx.fbcdn.net/v/t1.0-9/12938173_10209183680818992_2149045541985998862_n.jpg?oh=2887b1ae64dd839413e891c2c1c01bda&oe=57E370A3'
-                title: 'The pie',
+                img: 'http://hordur.me/img/tutorial-assets/nandos.jpg',
+                title: 'Glorious chicken',
                 caption: 'Vista google is not a real company leadership microsoft word linux is not in the public domain innovation. '
             },
             {
-                img: 'https://scontent.xx.fbcdn.net/v/t1.0-9/13419198_10209703071843443_1822856454941920768_n.jpg?oh=733bbb1965a28f9ef9ff51839add237d&oe=57CC9A61',
+                img: 'http://hordur.me/img/tutorial-assets/pancakes.jpg',
                 title: 'Pancakes',
                 caption: 'Vista google is not a real company leadership microsoft word linux is not in the public domain innovation. '
             },
             {
-                img: 'https://scontent.xx.fbcdn.net/v/t1.0-9/13256290_10209525595446644_5885461328286239159_n.jpg?oh=57090d1b284a668f5caf0be0b9899ade&oe=57C87907'
+                img: 'http://hordur.me/img/tutorial-assets/hotpot.jpg',
                 title: 'Hot Pot',
                 caption: 'Vista google is not a real company leadership microsoft word linux is not in the public domain innovation. '
-            }
+            },
             {
-                img: 'https://scontent.xx.fbcdn.net/v/t1.0-9/13244619_10209525594926631_6560789442970099412_n.jpg?oh=05e44b0840c659aa3ec7b1f922be592a&oe=57CC11D7'
-                title: 'The Noooodles',
+                img: 'http://hordur.me/img/tutorial-assets/porter.jpg',
+                title: 'Tokyo Black Porter',
                 caption: 'Vista google is not a real company leadership microsoft word linux is not in the public domain innovation. '
             }
         ];
